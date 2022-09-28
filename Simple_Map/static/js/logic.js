cca658e875b19722d9de24c19520cc33d26b9c34
melpatjen16 <mp.jenkins16@gmail.com>
2022-09-27 23:40:04 -0400

    Update

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -3,14 +3,14 @@ console.log("working");
 
 let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 25,
+    maxZoom: 18,
     accessToken: API_KEY
 });
 
 // We create the dark view tile layer that will be an option for our map.
 let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 25,
+    maxZoom: 18,
     accessToken: API_KEY
 });
 
@@ -23,7 +23,7 @@ let baseMaps = {
   // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
     center: [40.0, -80.0],
-    zoom: 2.5,
+    zoom: 2,
     layers: [light]
 })
 
@@ -33,9 +33,20 @@ L.control.layers(baseMaps).addTo(map);
 // Accessing the airport GeoJSON URL
 let torontoData = "https://raw.githubusercontent.com/melpatjen16/Mapping_Earthquakes/Mapping_GeoJSON_LineStrings/Simple_Map/torontoRoutes.json";
 
+// Create a style for the lines.
+let myStyle = {
+  color: "#ffffa1",
+  weight: 2
+}
 //Grabbing our GeoJSON data.
 d3.json(torontoData).then(function(data) {
     console.log(data);
   // Creating a GeoJSON layer with the retrieved data.
-  L.geoJSON(data).addTo(map);
+  L.geoJSON(data, {
+    style: myStyle,
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup("<h3> Airline: " + feature.properties.airline + "</h3><hr><h3> Destination: " + feature.properties.dst + "</h3>");
+    }
+  })
+.addTo(map);
 });
